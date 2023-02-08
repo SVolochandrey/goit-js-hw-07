e0796e395740474ef720a5340421ccf4b48768fb
SVolochandrey <volochan.andrey@gmail.com>
2023-02-08 18:18:26 +0200

    done 1st and 2nd tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,63 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const markupEl = createImageGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', markupEl);
+galleryContainer.addEventListener('click', onImageClick);
+
+function createImageGalleryMarkup (gallery) {
+return gallery
+.map(({preview, original, description}) => {
+return `<div class ="gallery__item">
+<a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt= "${description}"
+    />
+</a>
+</div>`;
+})
+.join("");
+}
+
+function onImageClick (event){
+event.preventDefault();
+
+if (event.target.nodeName !== "IMG") {
+return;
+}
+else {
+onOpenModalWindow(event);
+}
+}
+
+function onOpenModalWindow(event) {
+const instance = basicLightbox.create(
+`<img
+src = "${event.target.dataset.source}"
+width="800" 
+height="600"
+>`,
+{
+onShow: (instance) => {
+galleryContainer.addEventListener("keydown", onEscPress);
+},
+onClose: (instance) => {
+galleryContainer.removeEventListener("keydown", onEscPress);
+},
+}
+);
+
+instance.show();
+
+function onEscPress(event){
+if (event.code === "Escape"){
+instance.close();
+}
+}
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+const markupEl = createImageGalleryMarkup(galleryItems);
+galleryList.innerHTML = markupEl;
+
+function createImageGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}" 
+  alt="${description}" 
+  />
+</a>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
